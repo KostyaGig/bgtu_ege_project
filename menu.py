1eb91640751eca3924bb003c7cc6ee8910147423
Kostya Zinoview <k.gig@list.ru>
2023-02-24 08:31:34 +0300

    move and reuse select task data code

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 from print_utils import *
+from select_task_data import *
 
 TYPE_OF_TASK_KEY = "type_of_task"
 PASSWORD_LENGTH_KEY = "password_length"
@@ -39,7 +40,7 @@ def update_menu_path_if_need(item):
         print_menu_path()
 
 
-def call_configure_password_menu(item, task_configuration, is_recursive=True):
+def call_configure_password_menu(task_configuration, is_recursive=True):
     if is_recursive:
         menu_path.pop()
         print_menu_path()
@@ -60,35 +61,16 @@ def call_configure_password_menu(item, task_configuration, is_recursive=True):
     item_as_int = int(item)
 
     if item_as_int == 1:
-        print("\nEnter length of password: ")
-        length = str(input("length: "))
-
-        while not length.isnumeric() or (int(length) < 1):
-            length = str(input("enter corrected length of password in range(1, inf): "))
-        task_configuration[PASSWORD_LENGTH_KEY] = int(length)
+        task_configuration[PASSWORD_LENGTH_KEY] = select_and_return_type_password_length()
     elif item_as_int == 2:
-        print("\nEnter symbols of password: ")
-        symbols = str(input("symbols: "))
-
-        while not symbols:
-            symbols = str(input("enter at least 1 symbol: "))
-        task_configuration[PASSWORD_SYMBOLS_KEY] = set(symbols)
+        task_configuration[PASSWORD_SYMBOLS_KEY] = select_and_return_password_symbols()
     elif item_as_int == 3:
-        print("\nEnter count of passwords: ")
-        count = input("count: ")
-
-        while not count.isnumeric() or (int(count) < 1):
-            count = input("enter corrected count in range(1, inf): ")
-        task_configuration[COUNT_OF_PASSWORDS_KEY] = int(count)
+        task_configuration[COUNT_OF_PASSWORDS_KEY] = select_and_return_count_of_passwords()
     elif item_as_int == 4:
-        print("\nEnter extra info in bytes: ")
-        extra_info_in_bytes = input("extra_info_in_bytes: ")
+        task_configuration[EXTRA_INFO_KEY] = select_and_return_extra_info()
 
-        while not extra_info_in_bytes.isnumeric() or int(extra_info_in_bytes) < 1:
-            extra_info_in_bytes = input("extra_info_in_bytes(1, inf): ")
-        task_configuration[EXTRA_INFO_KEY] = int(extra_info_in_bytes)
+    if 1 <= item_as_int <= 4: call_configure_password_menu(task_configuration, is_recursive=True)
 
-    if 1 <= item_as_int <= 4: call_configure_password_menu(item, task_configuration, is_recursive=True)
 
 def call_start_menu(task_configuration, is_not_first_call=True):
     while len(menu_path) != 1:
@@ -121,7 +103,7 @@ def call_start_menu(task_configuration, is_not_first_call=True):
             task_configuration[TYPE_OF_TASK_KEY] = int(item)
         call_start_menu(task_configuration)
     else:
-        call_configure_password_menu(item, task_configuration, is_recursive=False)
+        call_configure_password_menu(task_configuration, is_recursive=False)
         call_start_menu(task_configuration)
 
 def configure_task_using_menu(task_configuration):
